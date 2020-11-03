31e71a260d3ac410eb361c650ae7699f3dd6a90c
sakilohale <19823319810@163.com>
2020-11-03 10:20:34 +0800

    完成了登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import login from '../components/login'
 import home from '../components/home'
+import welcome from "../components/welcome";
+import users from "../components/user/users";
 Vue.use(VueRouter)
 
 const routes = [
@@ -16,7 +18,17 @@ const routes = [
   },
   {
     path: '/home',
-    component: home
+    component: home,
+    redirect: '/welcome',
+    children:[
+      {
+      path:'/welcome',
+      component:welcome,
+    },
+      {
+        path: '/users',
+        component: users
+      }]
   }
 ]
 
